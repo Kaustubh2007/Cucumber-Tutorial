560a2355806dca0fc1d0b5ecf2bd9b21b6f98064
Kaustubh2007 <kaustubhbaitule@gmail.com>
2018-08-08 20:33:18 +0530

    added subtraction code

---
+++ src/arithmaticOperations/Differennce.java
@@ -0,0 +1,12 @@
+package arithmaticOperations;
+
+public class Differennce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 50, b = 30, c;
+		c=a-b;
+		System.out.println("Value of c is:" +c);
+	}
+
+}
